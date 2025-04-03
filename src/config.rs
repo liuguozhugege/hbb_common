4fe2e161b79969c0575a55dbb7e25407c88bb73b
liuguozhguege <156930495@qq.com>
2025-04-03 11:18:25 +0800

    修改中继服务器域名和key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["81.71.155.126"];
+pub const RS_PUB_KEY: &str = "HPRfrmCy8ztcLHLfA3R8lCizFQ1rJ9uIDdQeBARBEZc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
